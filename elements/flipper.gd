e7d4405068a3706f543b24b629ea14ab0d65f188
Jack Case <jackacase@gmail.com>
2023-10-03 21:14:32 -0400

    beginning conversion of flipper to a rigidbody

--- elements/flipper.gd
+++ elements/flipper.gd
@@ -14,6 +14,12 @@ func kill_current_tween():
 	if current_tween:
 		current_tween.kill()
 		current_tween = null
+		
+func flip():
+	pass
+	
+func return():
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,20 +31,8 @@ func _ready():
 
 func _input(event):
 	if Input.is_action_just_pressed(flipper_action):
-		kill_current_tween()
-		var flipper_fire_tween = self.create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
-		flipper_fire_tween.tween_property(self, "rotation", resting_rotation + Vector3(0, deg_to_rad(flip_angle if flipper_side == "left" else -flip_angle), 0), flip_duration)
-		current_tween = flipper_fire_tween
+		pass
 	
 	if Input.is_action_just_released(flipper_action):
-		kill_current_tween()
-		var return_duration = flip_duration * (abs(rad_to_deg(self.rotation.y))/flip_angle)
-		var flipper_return_tween = self.create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-		flipper_return_tween.tween_property(self, "rotation", resting_rotation, return_duration)
+		pass
 
-		
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
