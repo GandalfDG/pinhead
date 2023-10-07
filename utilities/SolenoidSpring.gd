305df5affce482012cf735e427d7d7cfaf6b893d
Jack Case <jackacase@gmail.com>
2023-10-07 11:37:34 -0400

    solenoid spring can be linear or rotational

--- utilities/SolenoidSpring.gd
+++ utilities/SolenoidSpring.gd
@@ -1,6 +1,10 @@
 @tool
 extends ActivatedPhysicsBehavior
 
+enum ForceType {LINEAR, ROTATIONAL} 
+
+@export var force_type: ForceType = ForceType.LINEAR
+
 @export var solenoid_force: float = 1
 @export var spring_stiffness: float = 500
 @export var resting_spring_extension: float = 10:
@@ -20,8 +24,13 @@ func _ready():
 func _process_physics_behavior(_delta):
 	# spring force is a function of the stiffness and the distance from the rest position
 	var spring_force = spring_stiffness * (parent_body.position * transform.basis - parent_rest_position * transform.basis + spring_extension_vector)
-	parent_body.apply_force(spring_force)
-	if activated:
-		parent_body.apply_impulse(-Vector3(0,0,solenoid_force) * transform.basis)
+	if force_type == ForceType.LINEAR:
+		parent_body.apply_force(spring_force)
+		if activated:
+			parent_body.apply_impulse(-Vector3(0,0,solenoid_force) * transform.basis)
+	else:
+		parent_body.apply_torque(spring_force)
+		if activated:
+			parent_body.apply_torque_impulse(-Vector3(0,0,solenoid_force) * transform.basis)
 		
 	super._process_physics_behavior(_delta)
