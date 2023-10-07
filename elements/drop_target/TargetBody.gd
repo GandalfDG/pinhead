0d1096d79c5ae2802b02921c71bfc9ee7eddc16e
Jack Case <jackacase@gmail.com>
2023-10-07 17:32:18 -0400

    starting on a drop target

---
+++ elements/drop_target/TargetBody.gd
@@ -0,0 +1,17 @@
+extends RigidBody3D
+
+signal BehaviorActivated(active)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(_delta):
+	if rotation.x < deg_to_rad(-0.05):
+		BehaviorActivated.emit(true)
+		print("drop hit")
+		
+#func _integrate_forces(state):
+#	if (state.transform.basis.get_euler() * global_transform.basis.inverse()).x < -0.5:
+#		print("drop hit")
