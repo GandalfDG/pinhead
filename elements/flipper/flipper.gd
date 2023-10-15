8e782ff9202b4472b8dd0d453e576b0f1ad3b946
Jack Case <jackacase@gmail.com>
2023-10-15 11:19:58 -0400

    Add debug function to track down flipper issue

--- elements/flipper/flipper.gd
+++ elements/flipper/flipper.gd
@@ -7,14 +7,39 @@ signal enable
 
 var flip_action: String
 @onready var actuator: SolenoidSpring = $FlipperBody/SolenoidSpring
+@onready var body: RigidBody3D = $FlipperBody
 
 func _ready():
 	flip_action = "left_flipper" if flipper_side == "left" else "right_flipper"
 	actuator.reverse_force = flipper_side == "right"
 	
-func _physics_process(delta):
+func mirror_flipper():
+	# if it's a right flipper, mirror everything
+	pass
+	
+	
+func _physics_process(_delta):
 	if Input.is_action_just_pressed(flip_action):
 		flipping.emit(true)
 	if Input.is_action_just_released(flip_action):
 		flipping.emit(false)
+	
+	debug_flipper_velocity(body.angular_velocity)
 
+
+###### Debugging stuff #######
+
+var debug_max_flipper_velocity: float = 0
+var debug_min_flipper_velocity: float = 10000000
+
+func debug_flipper_velocity(velocity: Vector3):
+	var local_velocity: Vector3 = velocity * global_transform.basis
+	var y_velocity: float = (local_velocity * Vector3.UP).y
+
+	if y_velocity > debug_max_flipper_velocity:
+		debug_max_flipper_velocity = y_velocity
+		print(self.name + " max velocity = " + str(debug_max_flipper_velocity))
+
+	if y_velocity < debug_min_flipper_velocity:
+		debug_min_flipper_velocity = y_velocity
+		print(self.name + " min velocity = " + str(debug_min_flipper_velocity))
