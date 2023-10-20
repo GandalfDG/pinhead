37c1ced86304f60bcb51b19ea10be987f9f1725f
Jack Case <jackacase@gmail.com>
2023-10-20 15:34:15 -0400

    cleaning up the playfield_inserts directory
    It's looking really good but I need to convert the VisualShader
    to a text Shader so that I can define per-instance uniforms.
    Otherwise all of the inserts will have the same colors and other
    parameters

--- elements/playfield_inserts/insert.gd
+++ elements/playfield_inserts/insert.gd
@@ -8,8 +8,8 @@ class_name InsertBase
 		top_layer_material.set_shader_parameter("BaseColor", color)
 		base_color = color
 
-@export var pattern_layer_material: ShaderMaterial = preload("res://elements/playfield_inserts/materials/insert_shader.material")
-@export var top_layer_material: ShaderMaterial = preload("res://elements/playfield_inserts/materials/insert_top_layer_material.tres")
+@export var pattern_layer_material: ShaderMaterial = preload("res://elements/playfield_inserts/materials/insert_pattern.material")
+@export var top_layer_material: ShaderMaterial = preload("res://elements/playfield_inserts/materials/insert_top_layer.material")
 
 @onready var pattern_layer_mesh: MeshInstance3D = $PatternedSurface
 @onready var top_layer_mesh: MeshInstance3D = $FlatSurface
