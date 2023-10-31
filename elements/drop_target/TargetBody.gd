9222cfbfffd4c4ad6a7cb1a2ef0da2a952e5b682
Jack Case <jackacase@gmail.com>
2023-10-30 21:36:47 -0400

    drop target fixing

--- elements/drop_target/TargetBody.gd
+++ elements/drop_target/TargetBody.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 signal BehaviorActivated(active)
+signal trigger_score
 
 @onready var joint: JoltGeneric6DOFJoint3D = $"../TiltAndSlideJoint"
 #@onready var reset_solenoid = $ResetSolenoid
@@ -25,6 +26,7 @@ func _physics_process(_delta):
 	
 	if not dropped and position.y < -0.5:
 		dropped =  true
+		trigger_score.emit()
 		
 	if  dropped and position.y > -0.5:
 		dropped = false
