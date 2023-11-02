b3acea453250df172d3994ccc99c3f1118eb9e2d
Jack Case <jackacase@gmail.com>
2023-11-02 11:24:57 -0400

    switching back to godot physics to see if I can get the same results

--- elements/drop_target/TargetBody.gd
+++ elements/drop_target/TargetBody.gd
@@ -3,26 +3,29 @@ extends RigidBody3D
 signal BehaviorActivated(active)
 signal trigger_score
 
-@onready var joint: JoltGeneric6DOFJoint3D = $"../TiltAndSlideJoint"
+@onready var joint: Generic6DOFJoint3D = $"../TiltAndSlideJoint"
 #@onready var reset_solenoid = $ResetSolenoid
 
 var dropped: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, 0)
+#	joint.set_param_y(Generic6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, 0)
+	pass
 
 func _unhandled_input(event):
 	if event.is_action_pressed("debug_reset"):
 		reset()
 
 func reset():
-	joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, 0)
+	pass
+#	joint.set_param_y(Generic6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, 0)
 
 func _physics_process(_delta):
 	if rotation.x < deg_to_rad(-0.08):
+		pass
 		# drop hit
-		joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, 1.51)
+#		joint.set_param_y(Generic6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, 1.51)
 	
 	if not dropped and position.y < -0.5:
 		dropped =  true
