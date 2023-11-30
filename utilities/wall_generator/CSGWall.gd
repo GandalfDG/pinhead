8ca4d64022aa11ab837a2005af48f31bcbee32f2
Jack Case <jackacase@gmail.com>
2023-11-29 20:58:55 -0500

    created basis for CSGWall node tool script

---
+++ utilities/wall_generator/CSGWall.gd
@@ -0,0 +1,18 @@
+@tool
+extends Path3D
+class_name CSGWall
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_curve_changed():
+	print("curve changed")
+	pass # Replace with function body.
