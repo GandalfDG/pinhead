6de941ab5347ab57a1e1fd592c32782237eab080
Jack Case <jackacase@gmail.com>
2023-11-30 22:23:15 -0500

    adding nodes and codes

--- utilities/wall_generator/CSGWall.gd
+++ utilities/wall_generator/CSGWall.gd
@@ -2,9 +2,11 @@
 extends Path3D
 class_name CSGWall
 
+@onready var wall_geometry = $WallGeometry
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	wall_geometry.path_node = ".."
 	pass # Replace with function body.
 
 
@@ -14,5 +16,6 @@ func _process(delta):
 
 
 func _on_curve_changed():
+	# pass the curve as the CSGPolygon's path node
 	print("curve changed")
 	pass # Replace with function body.
