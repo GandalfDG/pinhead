ed73966222c225c68630cde4d9273949d1f0ffb7
Jack Case <jackacase@gmail.com>
2024-01-13 20:15:22 -0500

    generates ramps with lateral edges parallel to the floor

--- utilities/ramp_generator/RampGenerator.gd
+++ utilities/ramp_generator/RampGenerator.gd
@@ -13,8 +13,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
 	pass
+
+func create_curve_point_basis(point_a: Vector3, point_b: Vector3) -> Basis:
+	# create a basis from a vector between point a and point b and the tilt angle
+	var curve_direction_vector = point_b - point_a
+	# project the vector onto the x-z plane to become the forward axis of the first basis
+	var projection = curve_direction_vector
+	projection.y = 0
+	projection = projection.normalized()
+
+	var curve_point_basis = Basis().looking_at(projection * -1, Vector3.DOWN, false)
+
+	var tilt_angle = curve_point_basis.z.angle_to(curve_direction_vector)
+	print(rad_to_deg(tilt_angle))
+	curve_point_basis = curve_point_basis.rotated(Vector3.LEFT, tilt_angle)
+	print(curve_point_basis)
+	return curve_point_basis
 
 func generate_ramp_surface():
 	var ramp_surface_curve: Curve3D = ramp_surface.curve
@@ -24,8 +39,7 @@ func generate_ramp_surface():
 
 	var st: SurfaceTool = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	#for i in ramp_surface_points.size() - 2:
-	for i in range(1):
+	for i in ramp_surface_points.size() - 2:
 		var point_a = ramp_surface_points[i]
 		var point_a_up = ramp_surface_up_vectors[i]
 		var point_a_tilt = ramp_surface_tilts[i]
@@ -34,53 +48,31 @@ func generate_ramp_surface():
 		var point_b_tilt = ramp_surface_tilts[i + 1]
 		var point_c = ramp_surface_points[i + 2]
 
-		
-		# create a basis from a vector between point a and point b and the tilt angle
-		var curve_direction_vector = point_b - point_a
-		# project the vector onto the x-z plane to become the forward axis of the first basis
-		var projection = curve_direction_vector
-		projection.y = 0
-		projection = projection.normalized()
-		print(projection)
-		var curve_point_basis_a = Basis().looking_at(projection * -1, Vector3.DOWN, false)
-		print(curve_point_basis_a)
-		var tilt_angle = curve_point_basis_a.z.angle_to(curve_direction_vector)
-		print(rad_to_deg(tilt_angle))
-		#curve_point_basis_a = curve_point_basis_a.rotated(curve_point_basis_a.x, tilt_angle)
-		
-		
-
-		curve_direction_vector = point_c - point_b
-		var curve_point_basis_b = Basis.looking_at(curve_direction_vector, Vector3(1, 0, 1))
-		
-
-
-		# curve_point_basis = curve_point_basis.rotated(Vector3.FORWARD * curve_point_basis, point_a_tilt)
+		var curve_point_basis_a = create_curve_point_basis(point_a, point_b)
+		var curve_point_basis_b = create_curve_point_basis(point_b, point_c)
 
 		# create a vector representing a movement from the curve point to
 		# the right ramp edge point and transform it by the basis
 		var right_edge_point_1 = Vector3(1, 0, 0) * curve_point_basis_a + point_a
 		var left_edge_point_1 = Vector3(-1, 0, 0) * curve_point_basis_a + point_a
+
 		var right_edge_point_2 = Vector3(1, 0, 0) * curve_point_basis_b + point_b
 		var left_edge_point_2 = Vector3(-1, 0, 0) * curve_point_basis_b + point_b
 
 		# generate a rectangle along the curve by creating two triangles
 		st.add_vertex(right_edge_point_1)
-		print(right_edge_point_1)
 		st.add_vertex(left_edge_point_1)
-		print(left_edge_point_1)
-		st.add_vertex(point_b)
-		#st.add_vertex(right_edge_point_2)
-		#st.add_vertex(right_edge_point_2)
-		#st.add_vertex(left_edge_point_1)
-		#st.add_vertex(left_edge_point_2)
+		st.add_vertex(left_edge_point_2)
+		
+		st.add_vertex(right_edge_point_1)
+		st.add_vertex(left_edge_point_2)
+		st.add_vertex(right_edge_point_2)
 
 	st.generate_normals()
 	st.generate_tangents()
 	var mesh = st.commit()
 	ramp_surface_mesh.set_mesh(mesh)
 
-	pass
 
 func _on_ramp_surface_path_curve_changed():
 	generate_ramp_surface()
