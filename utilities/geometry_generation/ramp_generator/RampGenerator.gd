eab961300a57feb48645c0792578b4c57b69747b
Jack Case <jackacase@gmail.com>
2024-01-15 15:49:09 -0500

    fixed signs of the curve bases
    still need to work on the tilt angle, as it isn't always correct

--- utilities/geometry_generation/ramp_generator/RampGenerator.gd
+++ utilities/geometry_generation/ramp_generator/RampGenerator.gd
@@ -18,22 +18,16 @@ class_name RampGenerator
 @onready var ramp_exit: Marker3D = $RampSurfacePath/RampExitPoint
 
 var curve_point_basis_cache: Dictionary
-var debug_markers: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func clear_debug_markers():
-	# for marker in debug_markers:
-	# 	marker.queue_free()
-
-	debug_markers = Array()
 
 func create_curve_point_basis(point_a: Vector3, point_b: Vector3, tilt: float) -> Basis:
 	var curve_point_basis = curve_point_basis_cache.get(point_a)
@@ -45,11 +39,12 @@ func create_curve_point_basis(point_a: Vector3, point_b: Vector3, tilt: float) -
 		projection.y = 0
 		projection = projection.normalized()
 
-		curve_point_basis = Basis.looking_at(projection * -1, Vector3.DOWN, false)
+		curve_point_basis = Basis.looking_at(projection, Vector3.UP, false)
 
-		var tilt_angle = curve_point_basis.z.angle_to(curve_direction_vector)
-		curve_point_basis = curve_point_basis.rotated(Vector3.LEFT, tilt_angle)
-		curve_point_basis = curve_point_basis.rotated(Vector3.FORWARD, tilt)
+		var tilt_angle = projection.angle_to(curve_direction_vector)
+
+		curve_point_basis = curve_point_basis.rotated(curve_point_basis.x, tilt_angle)
+		curve_point_basis = curve_point_basis.rotated(curve_point_basis.z, -tilt)
 		curve_point_basis_cache[point_a] = curve_point_basis
 
 	return curve_point_basis
@@ -59,14 +54,13 @@ func generate_ramp_surface():
 	# reset basis cache
 	curve_point_basis_cache = Dictionary()
 
-	clear_debug_markers()
-
 	var ramp_surface_curve: Curve3D = ramp_surface.curve
 	var ramp_surface_points: PackedVector3Array = ramp_surface_curve.get_baked_points()
 	var ramp_surface_tilts: PackedFloat32Array = ramp_surface_curve.get_baked_tilts()
 
 	var st: SurfaceTool = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	#for i in range(0,3):
 	for i in ramp_surface_points.size() - 2:
 		var point_a = ramp_surface_points[i]
 		var point_a_tilt = ramp_surface_tilts[i]
@@ -78,25 +72,22 @@ func generate_ramp_surface():
 		var curve_point_basis_a = create_curve_point_basis(point_a, point_b, point_a_tilt)
 		var curve_point_basis_b = create_curve_point_basis(point_b, point_c, point_b_tilt)
 		
-		var debug_marker = Marker3D.new()
-		debug_marker.set_transform(Transform3D(curve_point_basis_a, point_a))
-		debug_marker.set_owner(self)
-		self.add_child(debug_marker)
-		debug_markers.append(debug_marker)
 		# create a vector representing a movement from the curve point to
 		# the right ramp edge point and transform it by the basis
-		var right_edge_point_1 = Vector3(1, 0, 0) * curve_point_basis_a + point_a
-		var left_edge_point_1 = Vector3(-1, 0, 0) * curve_point_basis_a + point_a
+		var right_edge_point_1 = curve_point_basis_a * Vector3(1, 0, 0) + point_a
 
-		var right_edge_point_2 = Vector3(1, 0, 0) * curve_point_basis_b + point_b
-		var left_edge_point_2 = Vector3(-1, 0, 0) * curve_point_basis_b + point_b
+		var left_edge_point_1 = curve_point_basis_a * Vector3(-1, 0, 0) + point_a
 
-		var ramp_thickness_vector = Vector3(0, ramp_base_thickness, 0)
+
+		var right_edge_point_2 = curve_point_basis_b * Vector3(1, 0, 0) + point_b
+		var left_edge_point_2 = curve_point_basis_b * Vector3(-1, 0, 0) + point_b
+
+		var ramp_thickness_vector = Vector3(0, -ramp_base_thickness, 0)
 		
-		var bottom_right_point_1 = right_edge_point_1 + ramp_thickness_vector * curve_point_basis_a
-		var bottom_left_point_1 = left_edge_point_1  + ramp_thickness_vector * curve_point_basis_a
-		var bottom_right_point_2 = right_edge_point_2  + ramp_thickness_vector * curve_point_basis_b
-		var bottom_left_point_2 = left_edge_point_2  + ramp_thickness_vector * curve_point_basis_b
+		var bottom_right_point_1 = right_edge_point_1 + curve_point_basis_a * ramp_thickness_vector 
+		var bottom_left_point_1 = left_edge_point_1 + curve_point_basis_a * ramp_thickness_vector
+		var bottom_right_point_2 = right_edge_point_2 + curve_point_basis_b * ramp_thickness_vector
+		var bottom_left_point_2 = left_edge_point_2 + curve_point_basis_b * ramp_thickness_vector
 
 		# debug
 
@@ -138,7 +129,7 @@ func generate_ramp_surface():
 		
 		# start endcap
 		if i == 0:
-			ramp_entrance.transform = Transform3D(curve_point_basis_a, point_a)
+			ramp_entrance.global_transform = Transform3D(curve_point_basis_a, point_a)
 			st.add_vertex(right_edge_point_1)
 			st.add_vertex(bottom_right_point_1)
 			st.add_vertex(bottom_left_point_1)
@@ -149,7 +140,7 @@ func generate_ramp_surface():
 			
 		# end endcap
 		if i == ramp_surface_points.size() - 3:
-			ramp_exit.transform = Transform3D(curve_point_basis_b, point_b)
+			ramp_exit.global_transform = Transform3D(curve_point_basis_b, point_b)
 			st.add_vertex(right_edge_point_2)
 			st.add_vertex(bottom_left_point_2)
 			st.add_vertex(bottom_right_point_2)
