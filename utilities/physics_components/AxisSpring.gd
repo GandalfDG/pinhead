10444870392963f65c6bdec9a24b6e07263c0d44
Jack Case <jackacase@gmail.com>
2024-03-10 21:49:53 -0400

    reimplement AxisSpring on new PhysicsBehavior

--- utilities/physics_components/AxisSpring.gd
+++ utilities/physics_components/AxisSpring.gd
@@ -1,6 +1,5 @@
 extends PhysicsBehavior
 
-
 @export var force_type: PhysicsTypes.ForceType = PhysicsTypes.ForceType.LINEAR
 
 @export var spring_stiffness: float = 500
@@ -10,18 +9,19 @@ var force_vector: Vector3 = Vector3.UP
 
 func _ready():
 	force_vector *= global_transform.basis
+	print(transform.basis)
+	print(force_vector)
 
 func _physics_process(_delta):
 
-	var basis = global_transform.basis
 	if force_type == PhysicsTypes.ForceType.LINEAR:
-		var spring_displacement = abs((parent_node.global_transform.origin - parent_rest_transform.origin).length())
+		var spring_displacement = abs((physics_body.transform.origin - body_rest_transform.origin).length())
 		var spring_force_vector = spring_stiffness * (spring_displacement + spring_preload) * force_vector
-		parent_node.apply_force(spring_force_vector)
+		physics_body.apply_force(spring_force_vector)
 
-	elif force_type == PhysicsTypes.ForceType.ROTATIONAL:
-		var resting_vector = parent_rest_transform.basis * Vector3.UP
-		var current_vector = parent_node.global_transform.basis * Vector3.UP
-		var spring_displacement = current_vector.angle_to(resting_vector)
-		var spring_force_vector = spring_stiffness * (spring_displacement + spring_preload) * force_vector
-		parent_node.apply_torque(spring_force_vector * parent_node.global_transform.basis)
+	# elif force_type == PhysicsTypes.ForceType.ROTATIONAL:
+	# 	var resting_vector = parent_rest_transform.basis * Vector3.UP
+	# 	var current_vector = parent_node.global_transform.basis * Vector3.UP
+	# 	var spring_displacement = current_vector.angle_to(resting_vector)
+	# 	var spring_force_vector = spring_stiffness * (spring_displacement + spring_preload) * force_vector
+	# 	parent_node.apply_torque(spring_force_vector * parent_node.global_transform.basis)
