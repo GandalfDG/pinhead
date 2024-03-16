d65adb4a7ace1cafadeaa0f3e63d541a170562e0
Jack Case <jackacase@gmail.com>
2024-03-16 19:04:16 -0400

    fix: transform resting origin by inverse of global transform basis

--- utilities/physics_components/GlobalLockRigidBody3D.gd
+++ utilities/physics_components/GlobalLockRigidBody3D.gd
@@ -9,22 +9,25 @@ class_name GlobalLockRigidBody
 @export var lock_rotation_y: bool = false
 @export var lock_rotation_z: bool = false
 
-@onready var resting_transform = self.transform
-@onready var resting_origin = self.global_transform.origin
-@onready var resting_euler = self.transform.basis.get_euler()
+@onready var resting_transform = self.global_transform
+@onready var resting_origin = self.global_transform.origin * global_transform.basis.inverse()
+@onready var resting_euler = self.global_transform.basis.get_euler()
 
 var loop_count = 0
 
+func _ready():
+	pass
+
 func _integrate_forces(state):
 	loop_count += 1
-	state.integrate_forces()
+	#state.integrate_forces()
 	
 	var state_transform = state.transform
 
 	var state_origin = state_transform.origin
 
 	# transform origin to local coordinates
-	var local_origin = state_origin * transform.basis.inverse()
+	var local_origin = state_origin * global_transform.basis.inverse()
 
 	local_origin = Vector3(
 		resting_origin.x if lock_translation_x else local_origin.x,
@@ -32,10 +35,10 @@ func _integrate_forces(state):
 		resting_origin.z if lock_translation_z else local_origin.z
 	)
 
-	state_origin = local_origin * transform.basis
+	state_origin = local_origin * self.global_transform.basis
 
 	var state_basis = state_transform.basis
-	var local_basis = state_basis * transform.basis.inverse()
+	var local_basis = state_basis * global_transform.basis.inverse()
 	var local_euler = local_basis.get_euler()
 
 	local_euler = Vector3(
@@ -44,7 +47,7 @@ func _integrate_forces(state):
 		resting_euler.z if lock_rotation_z else local_euler.z
 	)
 
-	state_basis = Basis.from_euler(local_euler * transform.basis)
+	state_basis = Basis.from_euler(local_euler * global_transform.basis)
 
 	state_transform = Transform3D(state_basis, state_origin)
 	if loop_count % 100 == 0:
