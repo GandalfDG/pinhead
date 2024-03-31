f9f2b81ed412bc5712d2527c1b492350715e92b2
Jack Case <jackacase@gmail.com>
2024-03-31 10:13:53 -0400

    add force_type and reverse_force to PhysicsBehavior
    This would otherwise have to be duplicated in a lot of places

--- utilities/physics_components/PhysicsBehavior.gd
+++ utilities/physics_components/PhysicsBehavior.gd
@@ -2,6 +2,13 @@ extends Node3D
 
 class_name PhysicsBehavior
 
+@export var force_type: PhysicsTypes.ForceType = PhysicsTypes.ForceType.LINEAR
+@export var reverse_force: bool = false
+
+var force_vector = Vector3.UP:
+	get: 
+		return -1 * force_vector if reverse_force else force_vector
+
 var body_rest_transform: Transform3D
 var physics_body: RigidBody3D:
 	set(body):
