b4271de9be7a2cd78e0b3ff27ed3bfecdb8b054b
Jack Case <jackacase@gmail.com>
2024-04-07 14:14:50 -0400

    reimplemented flipper with rewritten physics nodes

--- elements/flipper/flipper.gd
+++ elements/flipper/flipper.gd
@@ -6,15 +6,20 @@ signal enable
 @export_enum("left", "right") var flipper_side: String = "left":
 	set(side):
 		flipper_side = side
-		flip_action = "left_flipper" if flipper_side == "left" else "right_flipper" 
-		actuator
+		set_flip_action()
+
 
 var flip_action: String
 @onready var body: RigidBody3D = $FlipperBody
 @onready var actuator: SolenoidSpring = $FlipperBody/SolenoidSpring
 
 func _ready():
+	actuator.init(body)
 	actuator.reverse_force = flipper_side == "right"
+	set_flip_action()
+	
+func set_flip_action():
+		flip_action = "left_flipper" if flipper_side == "left" else "right_flipper" 
 	
 func mirror_flipper():
 	# if it's a right flipper, mirror everything
@@ -22,12 +27,12 @@ func mirror_flipper():
 	
 	
 func _physics_process(_delta):
-	#if Input.is_action_just_pressed(flip_action):
+	if Input.is_action_just_pressed(flip_action):
+		actuator.activate(true)
 		#flipping.emit(true)
-	#if Input.is_action_just_released(flip_action):
+	if Input.is_action_just_released(flip_action):
+		actuator.activate(false)
 		#flipping.emit(false)
-		
-	pass
 	
 #	debug_flipper_velocity(body.angular_velocity)
 
